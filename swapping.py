16ef02c0bd562df9d6611efc16b86fbde1b402c8
Satyam1891 <128898729+Satyam1891@users.noreply.github.com>
2023-08-03 11:57:08 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,17 @@
+# swapping using only number
+"""a=int(input("enter the a: "))
+b=int(input("enter the b: "))
+a=a*b
+b=a//b
+a=a//b
+print("value of a and b" ,a,"&",b)
+"""
+
+#grade of the student
+g=int(input("enter the marks: "))
+if g > 60:
+      print("you are the above 60 percent")
+elif g<60 or g>33:
+    print("you are passed with second division")
+else:
+    print("you failed")      
