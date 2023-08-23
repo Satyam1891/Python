36fdc5522328eaa59d04cba54d5e298269939dc9
Satyam1891 <128898729+Satyam1891@users.noreply.github.com>
2023-08-23 22:17:29 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,11 @@
+def check(s1,s2):
+    if(sorted(s1)==sorted(s2)):
+        print("the strings are anagrams.")
+    else:
+        print("not anagram>")
+
+
+
+s1="listen"
+s2="silent"
+check(s1,s2)
